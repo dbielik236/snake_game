6adf1152bdcd7ff1710a3e89f24eb95978b5a46d
David Bielik <davidbielik@gmail.com>
2023-01-09 13:17:45 -0500

    added collisions and arrow key movement

--- snake_game.py
+++ snake_game.py
@@ -19,13 +19,14 @@ class Snake:
         for i in range(0, BODY_PARTS):
             self.coordinates.append([0, 0])
 
-        for x, y in self.coordinates:
-            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = SNAKE_COLOR, tag = "snake")
+        for x,y in self.coordinates:
+            square = canvas.create_rectangle(x,y,x + SPACE_SIZE,y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
             self.squares.append(square)
+
 class Food:
     def __init__(self):
-        x = random.randint(0, (GAME_WIDTH/SPACE_SIZE)-1)*SPACE_SIZE
-        y = random.randint(0, (GAME_HEIGHT/SPACE_SIZE)-1)*SPACE_SIZE
+        x = random.randint(0,(GAME_WIDTH/SPACE_SIZE)-1)*SPACE_SIZE
+        y = random.randint(0,(GAME_HEIGHT/SPACE_SIZE)-1)*SPACE_SIZE
 
         self.coordinates=[x, y]
 
@@ -44,7 +45,7 @@ def next_turn(snake, food):
     elif direction == "right":
         x += SPACE_SIZE
     
-    snake.coordinates.insert(0, (x, y))
+    snake.coordinates.insert(0,(x,y))
 
     square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = SNAKE_COLOR)
 
@@ -61,12 +62,44 @@ def next_turn(snake, food):
         canvas.delete(snake.squares[-1])
         del snake.squares[-1]
 
+    if check_collisions(snake):
+        game_over()
+
+    else:
+        window.after(SPEED, next_turn, snake, food)
+
 
 def change_direction(new_direction):
-    pass
+    global direction
+
+    if new_direction == 'left':
+        if direction!='right':
+            direction = new_direction
+    elif new_direction == 'right':
+        if direction!='left':
+            direction = new_direction
+    elif new_direction == 'up':
+        if direction!='down':
+            direction = new_direction
+    elif new_direction == 'down':
+        if direction!='up':
+            direction = new_direction     
+
+       
 
 def check_collisions(snake):
-    pass
+    x, y = snake.coordinates[0]
+
+    if x < 0 or x >= GAME_WIDTH:
+        return True
+    elif y < 0 or y >= GAME_HEIGHT:
+        return True
+    
+    for body_part in snake.coordinates[1:]:
+        if x == body_part[0] and y == body_part[1]:
+            return True
+
+    return False
 
 def game_over():
     pass
@@ -96,10 +129,15 @@ y = int((screen_height/2) - (window_height/2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
+window.bind('<Left>', lambda event: change_direction('left'))
+window.bind('<Right>', lambda event: change_direction('right'))
+window.bind('<Up>', lambda event: change_direction('up'))
+window.bind('<Down>', lambda event: change_direction('down'))
+
+
 snake = Snake()
 food = Food()
 
 next_turn(snake, food)
-
 
 window.mainloop()
